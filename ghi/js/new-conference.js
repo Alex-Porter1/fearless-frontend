cbee1fab8c048cff30db96266d198e1f2891764e
Alex Porter <alpo1992@gmail.com.here>
2022-07-20 19:22:07 -0600

    .

--- ghi/js/new-conference.js
+++ ghi/js/new-conference.js
@@ -15,26 +15,27 @@ window.addEventListener('DOMContentLoaded', async () => {
                 selectTag.appendChild(option);
             }
             console.log(data);
+        
+            const formTag = document.getElementById('create-conference-form');
+            formTag.addEventListener('submit', async event => {
+            event.preventDefault();
+        
+            const formData = new FormData(formTag);
+            const json = JSON.stringify(Object.fromEntries(formData));
+                const conferenceUrl = 'http://localhost:8000/api/conferences/';
+                const fetchConfig = {
+                    method: "post",
+                    body: json,
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                };
+                const response = await fetch(conferenceUrl, fetchConfig);
+                if (response.ok) {
+                    formTag.reset();
+                    const newConference = await response.json();
+                    console.log(newConference);
+                }
+            });
         }
-        const formTag = document.getElementById('create-conference-form');
-        formTag.addEventListener('submit', async event => {
-        event.preventDefault();
-    
-        const formData = new FormData(formTag);
-        const json = JSON.stringify(Object.fromEntries(formData));
-            const conferenceUrl = 'http://localhost:8000/api/conferences/';
-            const fetchConfig = {
-                method: "post",
-                body: json,
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            };
-            const response = await fetch(conferenceUrl, fetchConfig);
-            if (response.ok) {
-                formTag.reset();
-                const newConference = await response.json();
-                console.log(newConference);
-            }
-        });
 });
--- ghi/js/new-location.js
+++ ghi/js/new-location.js
@@ -15,27 +15,28 @@ window.addEventListener('DOMContentLoaded', async () => {
                 selectTag.appendChild(option);
             }
             console.log(data);
-        }
 
-    const formTag = document.getElementById('create-location-form');
-    formTag.addEventListener('submit', async event => {
-    event.preventDefault();
 
-    const formData = new FormData(formTag);
-    const json = JSON.stringify(Object.fromEntries(formData));
-        const locationUrl = 'http://localhost:8000/api/locations/';
-        const fetchConfig = {
-            method: "post",
-            body: json,
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        };
-        const response = await fetch(locationUrl, fetchConfig);
-        if (response.ok) {
-            formTag.reset();
-            const newLocation = await response.json();
-            console.log(newLocation);
+            const formTag = document.getElementById('create-location-form');
+            formTag.addEventListener('submit', async event => {
+            event.preventDefault();
+            
+            const formData = new FormData(formTag);
+            const json = JSON.stringify(Object.fromEntries(formData));
+                const locationUrl = 'http://localhost:8000/api/locations/';
+                const fetchConfig = {
+                    method: "post",
+                    body: json,
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                };
+                const response = await fetch(locationUrl, fetchConfig);
+                if (response.ok) {
+                    formTag.reset();
+                    const newLocation = await response.json();
+                    console.log(newLocation);
+                }
+            });
         }
-    });
 });
