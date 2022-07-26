283385b53f47915e60001278653a2784c696740c
Alex Porter <alpo1992@gmail.com.here>
2022-07-26 16:58:40 -0600

    npm run build

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -11,7 +11,7 @@ function Nav() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-            <NavLink className="nav-link" aria-current="page" to="#">
+            <NavLink className="nav-link" aria-current="page" to="/">
               Home
             </NavLink>
             </li>
